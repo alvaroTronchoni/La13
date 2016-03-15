21655b8364e3d85855439d10351294464c57d5f3
Informatica <Informatica>
2016-03-15 09:40:34 +0100

    Initial commit

---
+++ app/src/main/java/com/la13/baysan/la13/activitys/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.la13.baysan.la13.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.la13.baysan.la13.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
